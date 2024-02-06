27e5143ac74dc3c964bc03b25df94ce1922d80d7
jmnieves12 <natureshadowlock723@gmail.com>
2024-02-05 16:11:45 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,32 @@
 
+num1 = int(input("Enter your num 1: "))
+op = input("Enter your operator: ")
+num2 = int(input("Enter your num 2: "))
+
+if op == "+":
+	total = num1 + num2
+	print(f"{num1} + {num2} = {total}")
+
+if op == "-":
+	total = num1 - num2
+	print(f"{num1} - {num2} = {total}")
+
+if op == "*":
+	total = num1 * num2
+	print(f"{num1} * {num2} = {total}")
+
+if op == "/":
+	total = num1 / num2
+	print(f"{num1} / {num2} = {total}")
+
+if op == "**":
+	total = num1 ** num2
+	print(f"{num1} ** {num2} = {total}")
+
+if op == "//":
+	total = num1 // num2
+	print(f"{num1} // {num2} = {total}")
+
+if op == "%":
+	total = num1 % num2
+	print(f"{num1} % {num2} = {total}")
